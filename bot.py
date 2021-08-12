74e7d0de088fc4d5437c4375ac38d032079fd803
kasu2002 <80556599+kasu2002@users.noreply.github.com>
2021-08-12 09:50:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,10 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Hi iam welcome messanger bot 
+ updater.message.reply_text('''Hi I am welcome messanger bot 
 Add me to your group 
  
- Made with Love ❤️ by @lntechnical
-
+ Develpoed by @Kasu_Bro
   ''')
 def help(updater,context):
  updater.message.reply_text("Add me to your group ")
